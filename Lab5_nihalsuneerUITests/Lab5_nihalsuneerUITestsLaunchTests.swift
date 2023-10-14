69e2f70fbb840202a0c4894366f60fd476797725
user235383 <user235383@NY546.local>
2023-10-13 22:57:35 -0400

    lab-5 story book

---
+++ Lab5_nihalsuneerUITests/Lab5_nihalsuneerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab5_nihalsuneerUITestsLaunchTests.swift
+//  Lab5_nihalsuneerUITests
+//
+//  Created by user235383 on 10/12/23.
+//
+
+import XCTest
+
+final class Lab5_nihalsuneerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
